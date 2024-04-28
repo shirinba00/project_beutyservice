ef4ca201cf7e016fd80e318ccdab1b6c5028c0eb
shirinba00 <shirinbakhtgosha60@gmail.com>
2024-04-28 21:09:17 +0330

    Create urls.py in app pages and add urls.py config

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
     path('', include("service.urls")),
+    path('', include("pages.urls")),
     
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
